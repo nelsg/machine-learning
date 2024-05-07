8666f45ac69fbef3a77a90ab38560bc5307a7948
GONCALVES Nelson (T0025615) <nelson.goncalves@thalesgroup.com>
2024-05-07 21:09:44 +0200

    add manim

---
+++ manim_phy.py
@@ -0,0 +1,24 @@
+from manim import *
+from manim_physics import *
+
+class StandingWaveExampleScene(Scene):
+    def construct(self):
+        wave1 = StandingWave(1)
+        wave2 = StandingWave(2)
+        wave3 = StandingWave(3)
+        wave4 = StandingWave(4)
+        waves = VGroup(wave1, wave2, wave3, wave4)
+        waves.arrange(DOWN).move_to(ORIGIN)
+        self.add(waves)
+        for wave in waves:
+            wave.start_wave()
+        self.wait()
+
+class LinearWaveExampleScene(ThreeDScene):
+    def construct(self):
+        self.set_camera_orientation(60 * DEGREES, -45 * DEGREES)
+        wave = LinearWave()
+        self.add(wave)
+        wave.start_wave()
+        self.wait()
+        wave.stop_wave()
